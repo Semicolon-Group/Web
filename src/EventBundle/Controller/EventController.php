d53f955176a77c0bff9d901d63a0b988d9a8b63e
vaider <besghaier.haithem@gmail.com>
2018-03-23 11:52:24 +0100

    afficher listes events

--- src/EventBundle/Controller/EventController.php
+++ src/EventBundle/Controller/EventController.php
@@ -4,17 +4,25 @@ namespace EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use BaseBundle\Entity\Event;
+
 
 class EventController extends Controller
 {
     /**
      * @Route("/", name="events")
      */
+
     public function eventsAction()
     {
-        return $this->render('EventBundle:Event:events.html.twig', array(
-            // ...
-        ));
+        $em=$this->getDoctrine();
+        $event=$em->getRepository(Event::class)->findAll();
+        return $this->render('EventBundle:Event:events.html.twig', array('events'=>$event));
     }
+
+
+
+
+
 
 }

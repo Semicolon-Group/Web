dd2e8eca4089621f6614117f436844563c4424b0
Seif Abdennadher <m.abdennadher.seif@gmail.com>
2018-04-02 17:49:47 +0100

    fixing some bugs, and rearranging MemberBundle

--- src/MemberBundle/Controller/MemberController.php
+++ src/MemberBundle/Controller/MemberController.php
@@ -68,26 +68,6 @@ class MemberController extends Controller
     }
 
     /**
-     * @Route("/profile/upload", name="upload_photo")
-     */
-    public function uploadAction(Request $request){
-        $photo = new Photo();
-        $user = $this->container->get('security.token_storage')->getToken()->getUser();
-        $form = $this->createForm(PhotoType::class, $photo);
-        $form->handleRequest($request);
-
-        if($form->isSubmitted()){
-            $photo->setType(\BaseBundle\Entity\Enumerations\PhotoType::Regular);
-            $photo->setUser($user);
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($photo);
-            $em->flush();
-            return new Response(Response::HTTP_OK);
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
      * @Route("/profile/edit", name="member_profile_edit")
      */
     public function editProfileAction(Request $request){
@@ -122,7 +102,7 @@ class MemberController extends Controller
 
 
     /**
-     * @Route("/update/about", name="update_about")
+     * @Route("/profile/updateAbout", name="update_about")
      */
     public function editAboutAction(Request $request){
         if($request->isXmlHttpRequest()){
@@ -136,7 +116,7 @@ class MemberController extends Controller
     }
 
     /**
-     * @Route("/other/{id}", name="other_profile")
+     * @Route("/profile/other/{id}", name="other_profile")
      */
     public function otherProfileAction($id){
         $connectedUser = $this->container->get('security.token_storage')->getToken()->getUser();
@@ -170,7 +150,7 @@ class MemberController extends Controller
     }
 
     /**
-     * @Route("/like", name="like_profile")
+     * @Route("/profile/like", name="like_profile")
      */
     public function likeProfileAction(Request $request){
         if($request->isXmlHttpRequest()){
@@ -188,7 +168,7 @@ class MemberController extends Controller
     }
 
     /**
-     * @Route("/dislike", name="dislike_profile")
+     * @Route("/profile/dislike", name="dislike_profile")
      */
     public function dislikeProfileAction(Request $request){
         if($request->isXmlHttpRequest()){
@@ -203,7 +183,7 @@ class MemberController extends Controller
     }
 
     /**
-     * @Route("/block/{id}", name="block_profile")
+     * @Route("/profile/block/{id}", name="block_profile")
      */
     public function blockProfileAction($id){
         $user = $this->container->get('security.token_storage')->getToken()->getUser();
@@ -229,259 +209,5 @@ class MemberController extends Controller
         $this->getDoctrine()->getManager()->flush();
 
         return $this->redirectToRoute('member_profile');
-    }
-
-    /**
-     * @Route("/photos", name="get_photos")
-     */
-    public function getPhotosAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $serializer = new Serializer(array(new ObjectNormalizer()));
-            $user = $this->container->get('security.token_storage')->getToken()->getUser();
-            $photos = $this->getDoctrine()->getRepository(Photo::class)->findBy(array('user'=>$user, 'type' => \BaseBundle\Entity\Enumerations\PhotoType::Regular));
-            $data = $serializer->normalize($photos);
-            return new JsonResponse($data);
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * @Route("/photo/delete", name="delete_photo")
-     */
-    public function deletePhotoAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $photo = $this->getDoctrine()->getRepository(Photo::class)->find($request->get('id'));
-            $this->getDoctrine()->getManager()->remove($photo);
-            $this->getDoctrine()->getManager()->flush();
-            return new Response(Response::HTTP_OK);
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * @Route("/photo/asCover", name="make_cover")
-     */
-    public function makeCoverAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $list = $this->getDoctrine()->getRepository(Photo::class)->findBy(array('type'=>\BaseBundle\Entity\Enumerations\PhotoType::Cover));
-            if(sizeof($list)!=0){
-                $oldCover = $list[0];
-                $oldCover->setType(\BaseBundle\Entity\Enumerations\PhotoType::Regular);
-                $this->getDoctrine()->getManager()->persist($oldCover);
-                $this->getDoctrine()->getManager()->flush();
-            }
-
-            $photo = $this->getDoctrine()->getRepository(Photo::class)->find($request->get('id'));
-            $photo->setType(\BaseBundle\Entity\Enumerations\PhotoType::Cover);
-            $this->getDoctrine()->getManager()->persist($photo);
-            $this->getDoctrine()->getManager()->flush();
-
-            return new Response(Response::HTTP_OK);
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * @Route("/photo/asProfile", name="make_profile")
-     */
-    public function makeProfileAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $list = $this->getDoctrine()->getRepository(Photo::class)->findBy(array('type'=>\BaseBundle\Entity\Enumerations\PhotoType::Profile));
-            if(sizeof($list)!=0){
-                $oldProfile = $list[0];
-                $oldProfile->setType(\BaseBundle\Entity\Enumerations\PhotoType::Regular);
-                $this->getDoctrine()->getManager()->persist($oldProfile);
-                $this->getDoctrine()->getManager()->flush();
-            }
-
-            $photo = $this->getDoctrine()->getRepository(Photo::class)->find($request->get('id'));
-            $photo->setType(\BaseBundle\Entity\Enumerations\PhotoType::Profile);
-            $this->getDoctrine()->getManager()->persist($photo);
-            $this->getDoctrine()->getManager()->flush();
-
-            return new Response(Response::HTTP_OK);
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * @Route("/photo/getCoverPhoto", name="get_cover_photo")
-     */
-    public function getCoverPhotoAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $serializer = new Serializer(array(new ObjectNormalizer()));
-            $user = $this->container->get('security.token_storage')->getToken()->getUser();
-            $coverList = $this->getDoctrine()->getRepository(Photo::class)->findBy(array('user'=>$user, 'type' => \BaseBundle\Entity\Enumerations\PhotoType::Cover));
-            if(sizeof($coverList)==0){
-                return null;
-            }else{
-                $data = $serializer->normalize($coverList[0]);
-                return new JsonResponse($data);
-            }
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * @Route("/photo/getProfilePhoto", name="get_profile_photo")
-     */
-    public function getProfilePhotoAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $serializer = new Serializer(array(new ObjectNormalizer()));
-            $user = $this->container->get('security.token_storage')->getToken()->getUser();
-            $profileList = $this->getDoctrine()->getRepository(Photo::class)->findBy(array('user'=>$user, 'type' => \BaseBundle\Entity\Enumerations\PhotoType::Profile));
-            if(sizeof($profileList)==0){
-                return null;
-            }else{
-                $data = $serializer->normalize($profileList[0]);
-                return new JsonResponse($data);
-            }
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * @Route("/answer/generate", name="generate_answer")
-     */
-    public function generateAnswerAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $normalizer = new ObjectNormalizer();
-            $normalizer->setCircularReferenceLimit(2);
-            // Add Circular reference handler
-            $normalizer->setCircularReferenceHandler(function ($object) {
-                return $object->getId();
-            });
-            $serializer = new Serializer(array($normalizer));
-            $user = $this->container->get('security.token_storage')->getToken()->getUser();
-            $answers = $this->getDoctrine()->getRepository(Answer::class)->findBy(array('user' => $user));
-            $answeredQuestions = array_map(function ($an){
-                return $an->getQuestion();
-            }, $answers);
-            $questions = $this->getDoctrine()->getRepository(Question::class)->findAll();
-
-            if(sizeof($answeredQuestions) != sizeof($questions)){
-                foreach ($answeredQuestions as $aq){
-                    for($i=0; $i<sizeof($questions); $i++){
-                        if($questions[$i]->getId() == $aq->getId()){
-                            unset($questions[$i]);
-                            break;
-                        }
-                    }
-                }
-
-
-                $questions = array_values($questions);
-                $questionIndex = rand ( 0 , sizeof($questions)-1 );
-                $question = $questions[$questionIndex];
-                $choices = $this->getDoctrine()->getRepository(Choice::class)->findBy(array('question' => $question));
-                $toSend = array('question' => $question, 'choices' => $choices);
-                $data = $serializer->normalize($toSend);
-
-            }else{
-                $data = null;
-            }
-            return new JsonResponse($data);
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * @Route("/answers", name="get_answers");
-     */
-    public function getAnswersAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $normalizer = new ObjectNormalizer();
-            $normalizer->setCircularReferenceLimit(2);
-            // Add Circular reference handler
-            $normalizer->setCircularReferenceHandler(function ($object) {
-                return $object->getId();
-            });
-            $serializer = new Serializer(array($normalizer));
-            $user = $this->container->get('security.token_storage')->getToken()->getUser();
-            $answers = $this->getDoctrine()->getRepository(Answer::class)->findBy(array('user'=>$user));
-            if(sizeof($answers)==0){
-                return null;
-            }else{
-                $data = $serializer->normalize($answers);
-                return new JsonResponse($data);
-            }
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-
-
-    /**
-     * @Route("/answers/add", name="add_answer");
-     */
-    public function addAnswerAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $dom = $request->get('answer');
-            $user = $this->container->get('security.token_storage')->getToken()->getUser();
-
-            $answer = new Answer();
-            $answer->setUser($user);
-            $answer->setQuestion($this->getDoctrine()->getRepository(Question::class)->find($dom['question_id']));
-            $answer->setSelectedChoice($this->getDoctrine()->getRepository(Choice::class)->find($dom['selected_choice_id']));
-            $answer->setImportance($dom['importance_id']);
-            $answer->setDate(new \DateTime());
-
-            $acceptableChoices = $dom['accepted_choices_ids'];
-            foreach ($acceptableChoices as $c){
-                $answer->addChoice($this->getDoctrine()->getRepository(Choice::class)->find($c));
-            }
-
-            $this->getDoctrine()->getManager()->persist($answer);
-            $this->getDoctrine()->getManager()->flush();
-            return new Response(Response::HTTP_OK);
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * @Route("/answers/check", name="check_answer");
-     */
-    public function checkAnswerAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $user = $this->container->get('security.token_storage')->getToken()->getUser();
-            $question = $this->getDoctrine()->getRepository(Question::class)->find($request->get('questionId'));
-            $answers = $this->getDoctrine()->getRepository(Answer::class)->findBy(array('question' => $question, 'user' => $user));
-            if(sizeof($answers)!=0){
-                $resp = "true";
-            }else{
-                $resp = "false";
-            }
-            return new Response($resp);
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * @Route("/answers/getQuestion", name="get_question");
-     */
-    public function getQuestionAction(Request $request){
-        if($request->isXmlHttpRequest()){
-            $normalizer = new ObjectNormalizer();
-            $normalizer->setCircularReferenceLimit(1);
-            // Add Circular reference handler
-            $normalizer->setCircularReferenceHandler(function ($object) {
-                return $object->getId();
-            });
-            $serializer = new Serializer(array($normalizer));
-
-            $question = $this->getDoctrine()->getRepository(Question::class)->find($request->get('questionId'));
-            $choices = $this->getDoctrine()->getRepository(Choice::class)->findBy(array('question' => $question));
-            foreach ($choices as $choice){
-                $question->addChoice($choice);
-            }
-            $topics = Topic::getEnumAsArray();
-            $importances = Importance::getEnumAsArray();
-
-            $toSend = ['question' => $question, 'topics' => $topics, 'importances' => $importances];
-
-            $data = $serializer->normalize($toSend);
-            return new JsonResponse($data);
-        }
-        return new Response(Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }

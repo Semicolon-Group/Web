987c8c130946b5a85d53b90b058f27ac4946adfd
badis <mohamedbadis.maalej@esprit.tn>
2018-04-10 12:30:00 +0100

    files

--- src/Admin/BusinessBundle/Controller/AdvertController.php
+++ src/Admin/BusinessBundle/Controller/AdvertController.php
@@ -57,7 +57,11 @@ class AdvertController extends Controller
         )])
             ->add('payed',ChoiceType::class,[
             'choices' => ['Payed' => '1', 'Not payed' => '0']
-        ])->add('reason',TextType::class) ->add('Valider',SubmitType::class);;
+        ])->add('reason',TextType::class,[ 'attr' => array(
+
+                'required' => false
+
+            )]) ->add('Valider',SubmitType::class);;
         $form->handleRequest($request);
         if ($form->isSubmitted() )
         {

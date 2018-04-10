af2ea0e6c83d78a56b1de4e734d75e2456ebff03
Seif Abdennadher <m.abdennadher.seif@gmail.com>
2018-04-10 21:54:33 +0100

    Fixing alot of bugs :P

--- src/Business/AdvertBundle/Controller/BusinessAdvertController.php
+++ src/Business/AdvertBundle/Controller/BusinessAdvertController.php
@@ -36,7 +36,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Swift_Message;
 use Symfony\Component\Validator\Constraints\Date;
 
-require 'C:\xampp\htdocs\mysoulmate\vendor\autoload.php';
+require 'C:\wamp64\www\pi\mysoulmate\vendor\autoload.php';
 class BusinessAdvertController extends Controller
 {
     /**

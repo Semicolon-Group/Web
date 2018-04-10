ee79593b4ca60e8a4fb8ab1cd0beef24302df395
Seif Abdennadher <m.abdennadher.seif@gmail.com>
2018-04-10 13:27:57 +0100

    Fixing some sessions bugs

--- src/MemberBundle/Controller/MemberController.php
+++ src/MemberBundle/Controller/MemberController.php
@@ -65,45 +65,7 @@ class MemberController extends Controller
             'profile' => sizeof($profileList)==0?null:$profileList[0],
             'form' => $form->createView(),
             'available_questions_count' => $this->getAvailableQuestionCount(),
-            'diffs' => $this->getMandatory()
         ));
-    }
-
-    private function getMandatory(){
-        $user = $this->container->get('security.token_storage')->getToken()->getUser();
-        $data=null;
-        if(sizeof($this->getDoctrine()->getRepository(Answer::class)->getMandatoryAnswers($user))<10){
-            $normalizer = new ObjectNormalizer();
-            $normalizer->setCircularReferenceLimit(2);
-            // Add Circular reference handler
-            $normalizer->setCircularReferenceHandler(function ($object) {
-                return $object->getId();
-            });
-            $serializer = new Serializer(array($normalizer));
-            $answers = $this->getDoctrine()->getRepository(Answer::class)->findBy(array('user' => $user));
-            $answeredQuestions = array_map(function ($an){
-                return $an->getQuestion();
-            }, $answers);
-            $questions = $this->getDoctrine()->getRepository(Question::class)->findBy(array("topic" => Topic::Mandatory));
-
-            if(sizeof($answeredQuestions) != sizeof($questions)){
-                $diffQuestions = array_udiff($questions, $answeredQuestions,
-                    function ($obj_a, $obj_b) {
-                        return $obj_a->getId() - $obj_b->getId();
-                    }
-                );
-                $diffQuestions = array_values($diffQuestions);
-                if(sizeof($diffQuestions) != 0){
-                    $questionIndex = rand ( 0 , sizeof($diffQuestions)-1 );
-                    $question = $diffQuestions[$questionIndex];
-                    $choices = $this->getDoctrine()->getRepository(Choice::class)->findBy(array('question' => $question));
-                    $toSend = array('question' => $question, 'choices' => $choices);
-                    $data = $serializer->normalize($toSend);
-                }
-                return $diffQuestions;
-            }
-        }
-        return null;
     }
 
     /**

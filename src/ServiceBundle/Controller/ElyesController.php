7ac8c6eb3aed98ce243ce6eb5ee13c3625190815
Elyes <chebbaelyes@gmail.com>
2018-04-23 16:07:59 +0100

    - created elyesController for mobile services

---
+++ src/ServiceBundle/Controller/ElyesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ServiceBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ElyesController extends Controller
+{
+
+}
